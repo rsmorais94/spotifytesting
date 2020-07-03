3c0d22cb620dcebcf2ab1f2103e31abe5e8ffca3
rsmorais94 <rodilei.morais@gmail.com>
2020-07-03 16:44:39 -0300

    Updates in recommendation algorithm

--- recommendations.R
+++ recommendations.R
@@ -11,14 +11,12 @@ access_token <- get_spotify_access_token()
 ## Recommendations test
 my_top <- get_my_top_artists_or_tracks(
   type = "tracks",
-  time_range = "medium_term"
+  time_range = "short_term"
 )
 
-track_f <- get_track_audio_features(my_top[,"id"]
+track_f <- get_track_audio_features(my_top[,"id"])
 my_tracks <- left_join(my_top, track_f, by="id")
 
-ta = mean(my_tracks[["acousticness"]])
-test = my_tracks[1:5,"id"]
-test
-
-recs <- get_recommendations(seed_tracks = c("77oU2rjC5XbjQfNe3bD6so", "63SHcJzckFykS8faA5zojY", "6HV9d8u4WJ9lzbGrH5OQ6G", "6lK3nCiigk34SGqEjpQoqO", "3Hlh6tIHXS1H70YqoZ1Cka"))
+recs <- get_recommendations(seed_tracks = sample(my_tracks[,"id"], 5))
+to_buy <- (sample(recs[,"album.name"],3))
+print(to_buy)
